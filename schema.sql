8206e929683ba644a220b812e555b15ae33802b3
Mario Chois <mario.chois.navarro@gmail.com>
2021-09-20 16:22:59 -0500

    Table: Create animal table with columns: id (as primary key with autoincrement), name, date_of_birth, escape_attepmts, neutered, weight_kg

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
-);
+CREATE TABLE `vet_clinic`.`animals` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NULL,
+  `date_of_birth` DATE NULL,
+  `escape_attempts` INT NULL,
+  `neutered` TINYINT NULL,
+  `weight_kg` DECIMAL(2) NULL,
+  PRIMARY KEY (`id`));
